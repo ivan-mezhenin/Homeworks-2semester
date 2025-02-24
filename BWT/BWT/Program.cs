2ad6bafa6f7f7523b24ba2602a6753aa8568c736
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-02-24 17:34:04 +0300

    test: add tests

--- BWT/BWT/Program.cs
+++ BWT/BWT/Program.cs
@@ -1,5 +1,25 @@
 ﻿using BWT;
 
-var (word, position) = WordTransform.Transform("иван");
-Console.WriteLine($"{word}, {position}");
-Console.WriteLine($"{WordTransform.ReverseTransform(word, position)}");
+if (!Tests.TestsComplete())
+{
+    return;
+}
+
+Console.WriteLine("Write: '1 word' if you want transform word / '2 word position' - if you want reverse transform");
+
+switch (args[0])
+    {
+    case "1":
+        {
+            var answer = WordTransform.Transform(args[1]);
+            Console.WriteLine($"{answer.transformWord}: {answer.position}");
+            break;
+        }
+
+    case "2":
+        {
+            var answer = WordTransform.ReverseTransform(args[1], Convert.ToInt32(args[2]));
+            Console.WriteLine($"{answer}");
+            break;
+        }
+    }
