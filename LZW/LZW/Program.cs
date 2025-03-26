716dee187d4461b3cd6c00f6026c9ee689a3e435
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-03-26 23:12:19 +0300

    feat: add input processing in Main

--- LZW/LZW/Program.cs
+++ LZW/LZW/Program.cs
@@ -1,4 +1,24 @@
 ﻿using LZW;
 
-LZW.LZWEncode.Compress("test.txt");
-LZW.LZWDecode.Decompress("test.txt.zipped");
+Console.WriteLine("LZW. Write FilePath -c/-u. -c - if you want to compress file, -u - to decompress");
+
+switch (args[1])
+{
+    case "-c":
+    {
+        LZW.LZWEncode.Compress(args[0]);
+        break;
+    }
+
+    case "-u":
+    {
+        LZW.LZWDecode.Decompress(args[0]);
+        break;
+    }
+
+    default:
+    {
+        Console.WriteLine("Incorrect symbol");
+        break;
+    }
+}
