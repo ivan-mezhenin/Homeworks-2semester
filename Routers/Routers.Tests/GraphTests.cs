ab5112e430665a46ac26e7bc0bc75709185aa64d
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-04-11 13:46:30 +0300

    test: add test

--- Routers/Routers.Tests/GraphTests.cs
+++ Routers/Routers.Tests/GraphTests.cs
@@ -41,14 +41,34 @@ public class GraphTests
     }
 
     /// <summary>
+    /// test catch exception file not found.
+    /// </summary>
+    [Test]
+    public void Graph_ReadFormNonexistentFile_ShouldCatchException()
+    {
+        Assert.Throws<FileNotFoundException>(() =>
+        {
+            var graph = new Graph("NonexistentFile.txt");
+        });
+    }
+
+    /// <summary>
     /// test read graph from file.
     /// </summary>
     [Test]
     public void Graph_ReadGraphFromFile()
     {
         var graph = new Graph(GetTestFilePath("TestReadGraphFromFile.txt"));
+        var expectedGraph = new Graph();
 
-        Assert.That(graph.Edges[1][0].Neighbour, Is.EqualTo(2));
+        expectedGraph.Add(1, 2, 5);
+        expectedGraph.Add(2, 1, 5);
+        expectedGraph.Add(1, 3, 15);
+        expectedGraph.Add(3, 1, 15);
+        expectedGraph.Add(2, 4, 17);
+        expectedGraph.Add(4, 2, 17);
+
+        Assert.That(CompareGraph(graph, expectedGraph), Is.True);
     }
 
     private static string GetTestFilePath(string fileName)
@@ -64,5 +84,36 @@ public class GraphTests
         var fullPath = Path.Combine(directory!.FullName, "TestFiles", fileName);
 
         return fullPath;
+    }
+
+    private static bool CompareGraph(Graph graph1, Graph graph2)
+    {
+        if (graph1.Edges.Count != graph2.Edges.Count)
+        {
+            return false;
+        }
+
+        foreach (var (vertex, edges1) in graph1.Edges)
+        {
+            if (!graph2.Edges.TryGetValue(vertex, out var edges2))
+            {
+                return false;
+            }
+
+            if (edges1.Count != edges2.Count)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < edges1.Count; i++)
+            {
+                if (edges1[i] != edges2[i])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
