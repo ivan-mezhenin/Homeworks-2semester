28f1ae20d5467f72e0c7ed59f6d2769197f5f7ee
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-04-26 17:09:31 +0300

    feat: IndexOf

--- SkipList/SkipList/SList.cs
+++ SkipList/SkipList/SList.cs
@@ -1,161 +1,212 @@
-    // <copyright file="SList.cs" company="PlaceholderCompany">
-    // Copyright (c) PlaceholderCompany. All rights reserved.
-    // </copyright>
+// <copyright file="SList.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-    using System.Diagnostics;
+namespace SkipList;
 
-    namespace SkipList;
+using System.Collections;
 
-    using System.Collections;
+/// <summary>
+/// skip list.
+/// </summary>
+/// <typeparam name="T">type of list's elements.</typeparam>
+public class SList<T> : IList<T>
+    where T : IComparable<T>
+{
+    private const int MaxLevel = 32;
+    private readonly Random isLevelUp = new Random();
+    private readonly SkipListElement nil = new(default, null, null);
+    private SkipListElement head;
+    private SkipListElement bottomHead;
+    private int version;
 
     /// <summary>
-    /// skip list.
+    /// Initializes a new instance of the <see cref="SList{T}"/> class.
     /// </summary>
-    /// <typeparam name="T">type of list's elements.</typeparam>
-    public class SList<T> : IList<T>
-        where T : IComparable<T>
+    public SList()
     {
-        private const int MaxLevel = 32;
-        private readonly Random isLevelUp = new Random();
-        private readonly SkipListElement nil = new(default, null, null);
-        private SkipListElement head;
-        private SkipListElement bottomHead;
-        private int version;
+        this.bottomHead = new SkipListElement(default, this.nil, this.nil);
+        var current = this.bottomHead;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SList{T}"/> class.
-        /// </summary>
-        public SList()
+        for (var i = 1; i < MaxLevel; i++)
         {
-            this.bottomHead = new SkipListElement(default, this.nil, this.nil);
-            var current = this.bottomHead;
-
-            for (var i = 1; i < MaxLevel; i++)
-            {
-                current = new SkipListElement(default, this.nil, current);
-            }
-
-            this.head = current;
+            current = new SkipListElement(default, this.nil, current);
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SList{T}"/> class.
-        /// </summary>
-        /// <param name="collection">collection to initialization.</param>
-        public SList(IEnumerable<T> collection)
-        : this()
+        this.head = current;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SList{T}"/> class.
+    /// </summary>
+    /// <param name="collection">collection to initialization.</param>
+    public SList(IEnumerable<T> collection)
+    : this()
+    {
+        foreach (var item in collection)
         {
-            foreach (var item in collection)
-            {
-                this.Add(item);
-            }
-        }
-
-        /// <summary>
-        /// to get random level.
-        /// </summary>
-        /// <returns>level.</returns>
-        private int GetRandomLevel()
-        {
-            var level = 1;
-
-            while (this.isLevelUp.NextDouble() < 0.5 && level < MaxLevel)
-            {
-                ++level;
-            }
-
-            return level;
-        }
-
-        private class SkipListElement(T? value, SkipListElement? next, SkipListElement? down)
-        {
-            public T? Value { get; set; } = value;
-
-            public SkipListElement? Next { get; set; } = next;
-
-            public SkipListElement? Down { get; set; } = down;
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        /// <summary>
-        /// to add item in list.
-        /// </summary>
-        /// <param name="item">item to add.</param>
-        public void Add(T item)
-        {
-            var newLevel = this.GetRandomLevel();
-            var current = this.head;
-            SkipListElement? downNode = null;
-
-            for (var i = newLevel - 1; i >= 0; --i)
-            {
-                while (current.Next != null && current.Next != this.nil &&
-                       current.Next.Value != null &&
-                       current.Next.Value.CompareTo(item) < 0)
-                {
-                    current = current.Next;
-                }
-
-                var newNode = new SkipListElement(item, current.Next, i == 0 ? this.nil : downNode);
-                current.Next = newNode;
-                downNode = newNode;
-
-                current = current.Down ?? current;
-            }
-
-            ++this.Count;
-            ++this.version;
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count { get; private set; }
-        public bool IsReadOnly { get; }
-        public int IndexOf(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(int index, T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T this[int index]
-        {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            this.Add(item);
         }
     }
+
+    /// <summary>
+    /// to get random level.
+    /// </summary>
+    /// <returns>level.</returns>
+    private int GetRandomLevel()
+    {
+        var level = 1;
+
+        while (this.isLevelUp.NextDouble() < 0.5 && level < MaxLevel)
+        {
+            ++level;
+        }
+
+        return level;
+    }
+
+    private class SkipListElement(T? value, SkipListElement? next, SkipListElement? down)
+    {
+        public T? Value { get; set; } = value;
+
+        public SkipListElement? Next { get; set; } = next;
+
+        public SkipListElement? Down { get; set; } = down;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    /// <summary>
+    /// to add item in list.
+    /// </summary>
+    /// <param name="item">item to add.</param>
+    public void Add(T item)
+    {
+        var newLevel = this.GetRandomLevel();
+        var current = this.head;
+        SkipListElement? downNode = null;
+
+        for (var i = newLevel - 1; i >= 0; --i)
+        {
+            while (current.Next != null && current.Next != this.nil &&
+                   current.Next.Value != null &&
+                   current.Next.Value.CompareTo(item) < 0)
+            {
+                current = current.Next;
+            }
+
+            var newNode = new SkipListElement(item, current.Next, i == 0 ? this.nil : downNode);
+            current.Next = newNode;
+            downNode = newNode;
+
+            current = current.Down ?? current;
+        }
+
+        ++this.Count;
+        ++this.version;
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; private set; }
+    public bool IsReadOnly { get; }
+
+    /// <inheritdoc/>
+    public int IndexOf(T item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentNullException(nameof(item));
+        }
+
+        var index = 0;
+        var current = this.bottomHead.Next;
+
+        while (current != null && current != this.nil)
+        {
+            if (current.Value == null)
+            {
+                throw new InvalidOperationException("List contains null values");
+            }
+
+            var comparison = current.Value.CompareTo(item);
+            if (comparison == 0)
+            {
+                return index;
+            }
+
+            if (comparison > 0)
+            {
+                break;
+            }
+
+            current = current.Next;
+            ++index;
+        }
+
+        return -1;
+    }
+
+    /// <inheritdoc/>
+    public void Insert(int index, T item)
+        => throw new NotSupportedException("Insert by index is not supported in skip list.");
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            var current = this.bottomHead.Next ?? throw new EmptySkipListException("Attempt to take an item from empty list");
+
+            for (var i = 0; i <= index; ++i)
+            {
+                current = current.Next ?? throw new InvalidOperationException("Index is out of range.");
+            }
+
+            if (current.Value == null)
+            {
+                throw new InvalidOperationException("SkipList contains null value at specified index");
+            }
+
+            return current.Value;
+        }
+
+        set => throw new NotImplementedException();
+    }
+}
