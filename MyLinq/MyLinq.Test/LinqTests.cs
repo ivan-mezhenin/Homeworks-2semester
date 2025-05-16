3f0afeb05da922552c8b77f468b63db0c87b4275
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-05-16 20:49:47 +0300

    test: add test for GetPrimes()

---
+++ MyLinq/MyLinq.Test/LinqTests.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="LinqTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyLinq.Test;
+
+using MyLinq;
+
+/// <summary>
+/// test for linq.
+/// </summary>
+public class LinqTests
+{
+    /// <summary>
+    /// test method GetPrimes by taking first 5 numbers.
+    /// </summary>
+    [Test]
+    public void Linq_GetPrimes_FirstFiveNumbers()
+    {
+        Assert.That(Linq.GetPrimes().Take(5).ToList(), Is.EquivalentTo((int[])[2, 3, 5, 7, 11]));
+    }
+}
