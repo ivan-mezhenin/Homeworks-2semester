11db4140fec6c70ac90fcb14125c57a24944a7cf
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-05-16 21:04:41 +0300

    test: add test for Take

--- MyLinq/MyLinq.Test/LinqTests.cs
+++ MyLinq/MyLinq.Test/LinqTests.cs
@@ -17,6 +17,15 @@ public class LinqTests
     [Test]
     public void Linq_GetPrimes_FirstFiveNumbers()
     {
-        Assert.That(Linq.GetPrimes().Take(5).ToList(), Is.EquivalentTo((int[])[2, 3, 5, 7, 11]));
+        Assert.That(Linq.GetPrimes().Take(5), Is.EquivalentTo((int[])[2, 3, 5, 7, 11]));
+    }
+
+    /// <summary>
+    /// test method Take by taking first 4 elements of char array.
+    /// </summary>
+    [Test]
+    public void Linq_Take_FirstFourElements()
+    {
+        Assert.That(((char[])['a', 'b', 'c', 'd', 'e', 'f']).Take(4), Is.EquivalentTo((char[])['a', 'b', 'c', 'd']));
     }
 }
