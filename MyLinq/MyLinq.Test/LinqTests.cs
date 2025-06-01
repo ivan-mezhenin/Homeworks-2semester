44e98ee70edb4ee7cb860ddd67b0d1e4d6d71ee9
Ivan Mezhenin <mezhenin_ivan_06@mail.ru>
2025-06-01 22:35:28 +0300

    fix: head with license

--- MyLinq/MyLinq.Test/LinqTests.cs
+++ MyLinq/MyLinq.Test/LinqTests.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="LinqTests.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="LinqTests.cs" company="ivan-mezhenin">
+// Copyright (c) ivan-mezhenin. All rights reserved.
 // </copyright>
 
 namespace MyLinq.Test;
--- MyLinq/MyLinq/Linq.cs
+++ MyLinq/MyLinq/Linq.cs
@@ -1,5 +1,5 @@
-// <copyright file="Linq.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// <copyright file="Linq.cs" company="ivan-mezhenin">
+// Copyright (c) ivan-mezhenin. All rights reserved.
 // </copyright>
 
 namespace MyLinq;
